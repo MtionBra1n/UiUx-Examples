384db8bc8f854383eb1904dbc961095a431acc23
Rese <theresa.schlag@gmail.com>
2023-01-06 14:09:58 +0100

    fixed buttons

--- Assets/+++Workdata/#02 Button #2 Button Events/Scripts/ButtonEventManager.cs
+++ Assets/+++Workdata/#02 Button #2 Button Events/Scripts/ButtonEventManager.cs
@@ -8,6 +8,9 @@ public class ButtonEventManager : MonoBehaviour
     public Button button_options, button_loading, button_allClose;
     public GameObject container_loading, container_options;
 
+    /// Button Event on a Manager.
+    /// Pealse make your functions public, if you want to use the Button Event on Manager.
+    /// T.S.
     void Start()
     {
         button_options.onClick.AddListener(Button_Options);
@@ -15,19 +18,24 @@ public class ButtonEventManager : MonoBehaviour
         button_allClose.onClick.AddListener(Button_AllClose);
     }
 
-    void Button_Loading()
+
+    /// Button on Click via scipt.
+    /// Pealse make your functions public, if you want to use the Button Event on Manager.
+    /// T.S.
+
+    public void Button_Loading()
     {
         container_options.SetActive(false);
         container_loading.SetActive(true);
     }
 
-    void Button_Options()
+    public void Button_Options()
     {
         container_loading.SetActive(false);
         container_options.SetActive(true);
     }
 
-    void Button_AllClose()
+    public void Button_AllClose()
     {
         container_loading.SetActive(false);
         container_options.SetActive(false);
